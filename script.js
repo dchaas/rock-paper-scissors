f2308f90e18ceb58cf1194d2960ea9ca6ef88be8
Daniel Haas <daniel.c.haas@gmail.com>
2022-05-22 06:42:05 -0400

    Computer player added. Computer player will randomly return rock, scissors or paper

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Hello World");
+
+const OPTIONS = ['rock','paper','scissors'];
+
+function computerPlayer() {
+    let random = Math.floor(Math.random()*3);
+    return OPTIONS[random];
+}
