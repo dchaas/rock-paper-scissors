e188735974eb6b1d80ff861bd8a2e422b8ccd675
Daniel Haas <daniel.c.haas@gmail.com>
2022-05-22 07:08:47 -0400

    Full game functionality added. Helper function for getting user input added. Game made up of 5 rounds. Scoring handled by looking for "Won" or "Lost" in the playRound output.

--- script.js
+++ script.js
@@ -10,23 +10,51 @@ function computerPlayer() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     if (playerSelection === 'rock' && computerSelection == 'paper') {
-        return `You Lose! ${computerSelection} beats ${playerSelection}.`
+        return `You Lose! ${playerSelection} loses to ${computerSelection}.`
     } else if (playerSelection === 'rock' && computerSelection == 'scissors') {
         return `You Win! ${playerSelection} beats ${computerSelection}.`
     } else if (playerSelection === 'paper' && computerSelection == 'scissors') {
-        return `You Win! ${playerSelection} beats ${computerSelection}.`
+        return `You Lose! ${playerSelection} loses to ${computerSelection}.`
     } else if (playerSelection === 'paper' && computerSelection == 'rock') {
-        return `You Lose! ${computerSelection} beats ${playerSelection}.`
+        return `You Win! ${playerSelection} beats ${computerSelection}.`
     }  else if (playerSelection === 'scissors' && computerSelection == 'paper') {
         return `You Win! ${playerSelection} beats ${computerSelection}.`
     } else if (playerSelection === 'scissors' && computerSelection == 'rock') {
-        return `You Lose! ${computerSelection} beats ${playerSelection}.`
+        return `You Lose! ${playerSelection} loses to ${computerSelection}.`
     } else {
         return `Draw (${playerSelection} and ${computerSelection}), try again.`
     }
 
 }
 
-const playerSelection = 'rock';
-const computerSelection = computerPlayer();
-console.log(playRound(playerSelection,computerSelection));
+
+function getUserChoice() {
+    return prompt('Select rock, paper or scissors:','').toLowerCase();
+}
+
+
+function game() {
+    let user = 0;
+    let computer = 0;
+    // will play 5 rounds for a single game
+    for (let i=0; i<5; i++)  {
+        const playerSelection = getUserChoice();
+        const computerSelection = computerPlayer();
+        let result = playRound(playerSelection,computerSelection)
+        if (result.includes('Win')) {
+            user++;
+        } else if (result.includes('Lose')) {
+            computer++;
+        }
+        console.log(result);
+    }
+    if (user>computer) {
+        console.log(`You won the game ${user} to ${computer}!`)
+    } else if (user<computer) {
+        console.log(`You lost the game ${user} to ${computer}!`)
+    } else {
+        console.log(`Draw ${user} to ${computer}.`)
+    }
+}
+
+game();
