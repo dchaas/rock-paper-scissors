ba1bfd3bb7ca4e328450a84237e8d52984edbda6
Daniel Haas <daniel.c.haas@gmail.com>
2022-05-22 06:51:38 -0400

    playRound function generated. playRound function takes in the player and computer choices and displays the winner. Tested with hard-coded player choice.

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ function computerPlayer() {
     let random = Math.floor(Math.random()*3);
     return OPTIONS[random];
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === 'rock' && computerSelection == 'paper') {
+        return `You Lose! ${computerSelection} beats ${playerSelection}.`
+    } else if (playerSelection === 'rock' && computerSelection == 'scissors') {
+        return `You Win! ${playerSelection} beats ${computerSelection}.`
+    } else if (playerSelection === 'paper' && computerSelection == 'scissors') {
+        return `You Win! ${playerSelection} beats ${computerSelection}.`
+    } else if (playerSelection === 'paper' && computerSelection == 'rock') {
+        return `You Lose! ${computerSelection} beats ${playerSelection}.`
+    }  else if (playerSelection === 'scissors' && computerSelection == 'paper') {
+        return `You Win! ${playerSelection} beats ${computerSelection}.`
+    } else if (playerSelection === 'scissors' && computerSelection == 'rock') {
+        return `You Lose! ${computerSelection} beats ${playerSelection}.`
+    } else {
+        return `Draw (${playerSelection} and ${computerSelection}), try again.`
+    }
+
+}
+
+const playerSelection = 'rock';
+const computerSelection = computerPlayer();
+console.log(playRound(playerSelection,computerSelection));
