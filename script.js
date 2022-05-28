193cf432873ed1c031bb67defe1f35f6e7ba0ce6
Daniel Haas <daniel.c.haas@gmail.com>
2022-05-28 05:46:45 -0400

    Completed game. Added player and pc score tracking in a score div. Added check for winner when they hit 5 pts.

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const btnPaper = document.querySelector('.paper');
 const btnScissors = document.querySelector('.scissors');
 const btns = document.querySelectorAll('button');
 const results = document.querySelector("#results");
+const score = document.querySelector("#score");
+
+let userScore = 0;
+let pcScore = 0;
 
 btns.forEach((btn)=> {
     btn.addEventListener('click',
@@ -28,47 +32,41 @@ function computerPlayer() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     if (playerSelection === 'rock' && computerSelection == 'paper') {
+        pcScore++;
         results.textContent = `You Lose! ${playerSelection} loses to ${computerSelection}.`;
     } else if (playerSelection === 'rock' && computerSelection == 'scissors') {
+        userScore++;
         results.textContent = `You Win! ${playerSelection} beats ${computerSelection}.`;
     } else if (playerSelection === 'paper' && computerSelection == 'scissors') {
+        pcScore++;
         results.textContent =  `You Lose! ${playerSelection} loses to ${computerSelection}.`;
     } else if (playerSelection === 'paper' && computerSelection == 'rock') {
+        userScore++;
         results.textContent = `You Win! ${playerSelection} beats ${computerSelection}.`;
     }  else if (playerSelection === 'scissors' && computerSelection == 'paper') {
+        userScore++;
         results.textContent = `You Win! ${playerSelection} beats ${computerSelection}.`;
     } else if (playerSelection === 'scissors' && computerSelection == 'rock') {
+        pcScore++;
         results.textContent = `You Lose! ${playerSelection} loses to ${computerSelection}.`;
     } else {
         results.textContent =  `Draw (${playerSelection} and ${computerSelection}), try again.`;
     }
-
+    score.textContent = `Player = ${userScore} vs Computer = ${pcScore}`;
+    checkWinner();
 }
 
 
-
-function game() {
-    let user = 0;
-    let computer = 0;
-    // will play 5 rounds for a single game
-    for (let i=0; i<5; i++)  {
-        const playerSelection = getUserChoice();
-        const computerSelection = computerPlayer();
-        let result = playRound(playerSelection,computerSelection)
-        if (result.includes('Win')) {
-            user++;
-        } else if (result.includes('Lose')) {
-            computer++;
-        }
-        console.log(result);
-    }
-    if (user>computer) {
-        console.log(`You won the game ${user} to ${computer}!`)
-    } else if (user<computer) {
-        console.log(`You lost the game ${user} to ${computer}!`)
-    } else {
-        console.log(`Draw ${user} to ${computer}.`)
+function checkWinner() {
+    if (userScore===5) {
+        results.textContent =  `You WON!`;
+        results.style.color = "green";
+        results.style.fontWeight = "600";
+        results.style.fontSize='50px';
+    } else if (pcScore===5) {
+        results.textContent =  `You LOST!`;
+        results.style.color = "red";
+        results.style.fontWeight = "600";
+        results.style.fontSize='50px';
     }
 }
-
-game();
